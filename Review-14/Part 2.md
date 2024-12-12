47f5d4da6d6360d0b26478bbac6e248c36062a47
Navarone <klassifide@outlook.co.nz>
2024-12-12 13:59:30 +1300

    Final

---
+++ Review-14/Part 2.md
@@ -0,0 +1,16 @@
+An api is a connection between two computers or two programs on different computers.
+404, the requested data/page does not exist. 
+
+https://api.github.com:443/
+path = api.github.com
+port = 443
+protocol = https
+
+https://api.shopify.com/products?userId=5&page=2
+path = /products
+query = ?userId=5&page=2
+
+http://127.0.0.1/react
+127.0.0.1 = host
+
+4. Idk
--- Review-14/src/App.jsx
+++ Review-14/src/App.jsx
@@ -1,13 +1,39 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import Bitcoin from "./Bitcoin";
+import axios from 'axios';
+import BTC from './BTC.png';
+
 function App() {
-return (
-  <div className='Monies'>
-  <Bitcoin/>
-  </div>
-)
-}
+  const [bitcoinPrice, setBitcoinPrice] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchBitcoinPrice = async () => {
+      try {
+        const response = await axios.get('https://api.coindesk.com/v1/bpi/currentprice/BTC.json');
+        setBitcoinPrice(response.data.bpi.USD.rate);
+        setLoading(false);
+      } catch (err) {
+        setError('Approximately 100k');
+        setLoading(false);
+      }
+    };
+
+    fetchBitcoinPrice();
+  }, []);
+
+  if (loading) return <div>HACKED SCAMMED LOL420GF...</div>;
+  if (error) return <div>{error}</div>;
+  return (
+    <div className="Box">
+      <div>
+        <img src={BTC}/>
+        <h1 className="Golden">Current Bitcoin Price</h1>
+        </div>
+      <p>${bitcoinPrice}</p>
+    </div>
+  );
+};
+
 export default App;
-
-
